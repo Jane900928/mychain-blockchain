ac093761f544bd0c9fa82ab83292db390712b17e
Jane900928 <konglin928@gmail.com>
2025-08-18 22:32:56 +0800

    Add main.go

---
+++ cmd/mychaind/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/Jane900928/mychain-blockchain/app"
+	"github.com/Jane900928/mychain-blockchain/cmd/mychaind/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
